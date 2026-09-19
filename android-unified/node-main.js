c4c5c369de10f7e5e26220a9aba6383b2570ff43
E7G <55611737+E7G@users.noreply.github.com>
2026-09-20 02:01:09 +0800

    Run embedded server as ESM without changing business sources

--- android-unified/node-main.js
+++ android-unified/node-main.js
@@ -1,5 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const ts = require("typescript");
+const { pathToFileURL } = require("url");
+
 process.chdir(__dirname);
+
 const pkg = require("./package.json");
 process.env.npm_package_version = pkg.version;
-require("ts-node/register/transpile-only");
-require("./index.ts");
+
+const configSourcePath = path.join(__dirname, "config.ts");
+const configOutputPath = path.join(__dirname, "dist-mobile", "config.js");
+
+const configSource = fs.readFileSync(configSourcePath, "utf8");
+const transpiled = ts.transpileModule(configSource, {
+  compilerOptions: {
+    target: ts.ScriptTarget.ES2020,
+    module: ts.ModuleKind.ES2020,
+    esModuleInterop: true,
+    allowSyntheticDefaultImports: true,
+  },
+  fileName: "config.ts",
+  reportDiagnostics: true,
+});
+
+if (transpiled.diagnostics && transpiled.diagnostics.length > 0) {
+  const host = {
+    getCanonicalFileName: (fileName) => fileName,
+    getCurrentDirectory: () => __dirname,
+    getNewLine: () => "\n",
+  };
+  const message = ts.formatDiagnosticsWithColorAndContext(transpiled.diagnostics, host);
+  console.error(message);
+  process.exit(1);
+}
+
+fs.writeFileSync(configOutputPath, transpiled.outputText, "utf8");
+
+import(pathToFileURL(path.join(__dirname, "dist-mobile", "index.js")).href)
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
